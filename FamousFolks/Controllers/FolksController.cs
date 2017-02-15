5801c969231284f21216adf8f766bde67b652c07
Fattaneh2016 <fatameri@hotmail.com>
2017-02-15 01:53:37 -0500

    join 3 tables

--- FamousFolks/Controllers/FolksController.cs
+++ FamousFolks/Controllers/FolksController.cs
@@ -11,34 +11,76 @@ namespace FamousFolks.Controllers
         private FamousFolksEntities db = new FamousFolksEntities();
 
 
-
-
         // GET: Folks
-        public ActionResult Index(string searchTerm = null)
+        public ActionResult Index(string searchPolitics = null)
         {
 
-
-
             var query = db.Folks
-                .Where(r => searchTerm == null || r.LastName.StartsWith(searchTerm))
-                .OrderBy(r => r.LastName)
-                .Take(100)
-            .Select(r => new FolksListViewModel
-            {
-                Id = r.ID,
-                FirstName = r.FirstName,
-                LastName = r.LastName,
-                BirthLocation = r.BirthLocation,
-                Bio = r.Bio
-            });
+                .Join(db.FolksExpertises,
+                    a => a.ID,
+                    m => m.Id,
+                    (a, m) => new { a.FirstName, a.LastName, a.BirthLocation, a.Bio, m.ExpId })
+
+                    .Join(db.Expertises,
+                     a => a.ExpId,
+                    m => m.Id,
+                    (a, m) => new { a.FirstName, a.LastName, a.BirthLocation, a.Bio, m.Expertise1 })
+                    .Where(r => r.Expertise1 == "Politician" || searchPolitics == null)
+              .OrderBy(r => r.LastName)
+                .Select(r => new FolksListViewModel
+                {
+                    FirstName = r.FirstName,
+                    LastName = r.LastName,
+                    BirthLocation = r.BirthLocation,
+
+                    Expertise = r.Expertise1,
+                    Bio = r.Bio
+
+                });
+
+
 
             return View(query);
 
-            //return View(db.Folks.ToList());
+            //var query = db.Folks.Join(db.Expertises,
+            //    a => a.ID,
+            //    m => m.Id,
+            //    (a, m) => new { a.FirstName, a.LastName, a.BirthLocation, a.Bio, m.Expertise1 })
+            //    .Where(r => r.Expertise1 == "Politician" || searchPolitics == null)
+            //    .OrderBy(r => r.LastName)
+            //.Select(r => new FolksListViewModel
+            //{
+
+            //    FirstName = r.FirstName,
+            //    LastName = r.LastName,
+            //    BirthLocation = r.BirthLocation,
+            //    Expertise = r.Expertise1,
+            //    Bio = r.Bio
+
+            //});
+
+            //return View(query);
 
         }
 
+        //var query = db.Folks
+        //        .Where(r => searchTerm == null || r.LastName.StartsWith(searchTerm))
+        //        .OrderBy(r => r.LastName)
+        //        .Take(100)
+        //    .Select(r => new FolksListViewModel
+        //    {
+        //        Id = r.ID,
+        //        FirstName = r.FirstName,
+        //        LastName = r.LastName,
+        //        BirthLocation = r.BirthLocation,
+        //        Bio = r.Bio
+        //    });
 
+        //    return View(query);
+
+        //    //return View(db.Folks.ToList());
+
+        //}
 
         // GET: Folks/Details/5
         public ActionResult Details(int? id)
--- FamousFolks/Models/FolksListViewModel.cs
+++ FamousFolks/Models/FolksListViewModel.cs
@@ -6,7 +6,11 @@
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public string BirthLocation { get; set; }
+        public int? ExpertiseId { get; set; }
+
+        public string Expertise { get; set; }
         public string Bio { get; set; }
+
 
     }
 }
